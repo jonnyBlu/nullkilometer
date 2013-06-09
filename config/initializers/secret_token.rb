c4122f00187b005150fcae6bdc73dddb702c3695
jonnyBlu <j0nny.3lu@googlemail.com>
2013-06-09 16:27:07 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nullkilometer::Application.config.secret_token = '3b29237222278768a95457123b03df7ba5a7c9b5261a559c5dbeeb81b815b7719e2bba896d47c4e1463c9c043d8c6674e7cdfe94b2e8547fb0155820d9664d5f'
